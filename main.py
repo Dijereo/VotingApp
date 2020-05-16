8a100619244bba8254e51336fd4087217a83012e
Aaron-AB <aaronboodoo@live.com>
2020-05-16 20:02:35 +0000

    hope this works

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import dbproxy
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-    app.config['SECRET_KEY'] = randString(8)
+    app.config['SECRET_KEY'] = "PLEASEWORK"
     app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=7)
     db.init_app(app)
     return app
